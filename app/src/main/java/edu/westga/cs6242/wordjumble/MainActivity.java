8b2b2777fb8674b3a9e9dd7cb599f8e687ae29c2
twilli58 <twilli58@my.westga.edu>
2016-03-19 22:39:19 -0400

    adding the main background image, buttons and Icon to the game

--- app/src/main/java/edu/westga/cs6242/wordjumble/MainActivity.java
+++ app/src/main/java/edu/westga/cs6242/wordjumble/MainActivity.java
@@ -3,6 +3,7 @@ package edu.westga.cs6242.wordjumble;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar action = getSupportActionBar();
+        action.setDisplayShowHomeEnabled(true);
+        action.setIcon(R.mipmap.ic_launcher);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
